6ca812036f04703668cfbebde9894fbeaeb43c18
vi7ao <vitortelesmp@gmail.com>
2023-07-21 00:22:11 -0300

    add singleton persistent

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -24,3 +24,17 @@ public class Singleton<T> : MonoBehaviour
         }
     }
 }
+
+public class SingletonPersistent<T> : MonoBehaviour
+    where T : Component
+{
+    public static T Instance {get; private set;}
+    public virtual void Awake(){
+        if (Instance == null){
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
